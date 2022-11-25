f2a9b2ff70c702d8cbabf87d42b203dd57272f1d
Jonathan Sales <51421355+JonathanSaless@users.noreply.github.com>
2022-11-25 18:01:21 -0300

    Add files via upload

--- matriz-mpi.c
+++ matriz-mpi.c
@@ -1,22 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>		// para obter a hora do computador no srand
-#include <mpi.h>
-#include <unistd.h>
+#include <mpi.h>		//mpi
+#include <unistd.h>		//para pausar a execução do programa por um tempo
 
-const int ordem = 8;	//variavel global
+const int ordem = 40;	//variavel global
 
 //função que preenche a matriz com valores aleatórios
 void preencher_matriz(int m[][ordem])
 {
 	int i, j;
-	srand(time(NULL));		//semente padrão para geração de numeros aleatórios, time(NULL) para receber a hora do computador que sempre será diferente, logo sempre gerará uma semente padrão dirente ao executar o programa
+	srand(time(NULL));					//semente padrão para geração de numeros aleatórios, time(NULL) para receber a hora do computador que sempre será diferente, logo sempre gerará uma semente padrão dirente ao executar o programa
 	
 	for(i=0; i<ordem; i++)
 	{
 		for(j=0; j<ordem; j++)
 		{
-			m[i][j] = rand() % 100;		//numeros no maximo 99, pois utilizará sempre um valor que é resto de uma divisão por 100, que é no max 99
+			m[i][j] = rand() % 10;		//numeros no maximo 99, pois utilizará sempre um valor que é resto de uma divisão por 100, que é no max 99
+		}
+	}	
+}
+
+void preencher_matriz_zeros(int m[][ordem])
+{
+	int i, j;
+	
+	for(i=0; i<ordem; i++)
+	{
+		for(j=0; j<ordem; j++)
+		{
+			m[i][j] = 0;
 		}
 	}	
 }
@@ -38,12 +51,12 @@ void imprimir_matriz(int m[][ordem])
 }
 
 //função que soma duas matriz
-void somar_matrizes(int m1[][ordem], int m2[][ordem], int m_soma[][ordem], int p)
+void somar_matrizes(int m1[][ordem], int m2[][ordem], int m_soma[][ordem], int proc)
 {
 	int i, j, p_linha, p_coluna, u_linha, u_coluna;
 
-	//selecionando parte da matriz as matrizes
-	switch (p)
+	//selecionando parte da matriz
+	switch (proc)
 	{
 	case 0:
 		p_linha = 0;
@@ -86,162 +99,56 @@ void somar_matrizes(int m1[][ordem], int m2[][ordem], int m_soma[][ordem], int p
 	}
 }
 
-void rodar_mpi(){
-
-}
 int main(int argc, char** argv){
     
 	
-    int a[8][8] = 
-	{
-		{2, 3, 1, 4, 2, 3, 5, 4}, 
-		{3, 1, 2, 5, 2, 3, 4, 4}, 
-		{5, 6, 1, 6, 2, 3, 3, 4},
-		{4, 1, 3, 7, 2, 3, 4, 4},
-		{2, 3, 1, 4, 2, 3, 5, 4}, 
-		{3, 1, 2, 5, 2, 3, 7, 4}, 
-		{5, 6, 1, 6, 2, 3, 9, 4},
-		{4, 1, 3, 7, 2, 3, 1, 4}
-	};
-	
-	int b[8][8] = 
-	{
-		{4, 1, 1, 5, 2, 3, 1, 4},
-		{7, 3, 1, 1, 2, 3, 1, 4},
-		{5, 5, 6, 6, 2, 3, 1, 4},
-		{3, 9 ,1, 5, 2, 3, 1, 4},
-		{4, 1, 1, 5, 2, 3, 1, 4},
-		{7, 3, 1, 1, 2, 3, 1, 4},
-		{5, 5, 6, 6, 2, 3, 1, 4},
-		{3, 9 ,1, 5, 2, 3, 1, 4}
-	};
-	
-	int c[8][8] = 
-    	{
-	    	{0, 0, 0, 0, 0, 0, 0, 0},
-		    {0, 0, 0, 0, 0, 0, 0, 0},
-		    {0, 0, 0, 0, 0, 0, 0, 0},
-		    {0, 0, 0, 0, 0, 0, 0, 0},
-		    {0, 0, 0, 0, 0, 0, 0, 0},
-		    {0, 0, 0, 0, 0, 0, 0, 0},
-		    {0, 0, 0, 0, 0, 0, 0, 0},
-		    {0, 0, 0, 0, 0, 0, 0, 0}
-	    };
-
-    //inicialização do ambiente MPI
+    int a[40][40], b[40][40], c[40][40], bytes = 40*40;
+	preencher_matriz(a);
+	usleep(1000000);	//sleep time para que a matriz a ser preenchida em B, não seja identica a matriz A. As matrizes são geradas aleatoriamente em função do tempo.
+	preencher_matriz(b);
+	preencher_matriz_zeros(c);
+    
     MPI_Init(NULL, NULL);
     
-
     int world_size;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
     int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-    if (world_size != 4)        //necessário ser 4 processos, pois divide a matriz em 4 partes
+    if (world_size != 4)        				//necessário ser 4 processos, pois divide a matriz em 4 partes, se for diferente retorna um erro
     {
-        fprintf(stderr, "World size must be two for %s\n", argv[0]);
-        MPI_Abort(MPI_COMM_WORLD, 1);
+        printf("Numero de processos precisa ser igual a 4!\n");
+        MPI_Abort(MPI_COMM_WORLD, 1);			//aborta o MPI
     }
-
-    printf("RANK %d\n", world_rank);
     
-
-    /*if (world_rank != 0)
+    if (world_rank != 0)
     {
-        MPI_Recv(&c, 1, MPI_INT, world_rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        somar_matrizes(a, b, c, world_rank);
-        printf("***Matriz C***\n");
+        MPI_Recv(&c, bytes, MPI_INT, world_rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);		//se o processo for diferente de 0, recebe a matriz somada do processo anterior
+		printf("Processo atual: %d\nRecebido a matriz C do processo %d\n", world_rank, world_rank - 1);
+        somar_matrizes(a, b, c, world_rank);													//e soma as matrizes c1,2 (world_rank = 1), c2,1 (world_rank = 2), c2,2 (world_rank = 3)
+		printf("*************************************************************** Matriz C ***************************************************************\n");
     	imprimir_matriz(c);
     }
-    
-    MPI_Send(&c, 1, MPI_INT, (world_rank + 1) % world_size, 0, MPI_COMM_WORLD);
-    printf("SEND: RANK %d WORD SIZE %d\n", world_rank, (world_rank + 1) % world_size);
+	else																						//se for o processo 0 (world_rank = 0), realiza a soma das matrizes parte c1,1
+	{
+		somar_matrizes(a, b, c, world_rank);
+        printf("*************************************************************** Matriz C ***************************************************************\n");
+    	imprimir_matriz(c);
+	}
+	
+    MPI_Send(&c, bytes, MPI_INT, (world_rank + 1) % world_size, 0, MPI_COMM_WORLD);
     
     if (world_rank == 0) {
-        MPI_Recv(&c, 1, MPI_INT, world_size - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        somar_matrizes(a, b, c, world_rank);
-        printf("***Matriz C***\n");
+        MPI_Recv(&c, bytes, MPI_INT, world_size - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		printf("Processo atual: %d\nRecebido a matriz C do processo %d\n", world_rank, world_size-1);
+		printf("*************************************************************** Matriz A ***************************************************************\n");
+		imprimir_matriz(a);
+		printf("*************************************************************** Matriz B ***************************************************************\n");
+		imprimir_matriz(b);
+        printf("*************************************************************** Matriz C FINAL ***************************************************************\n");
     	imprimir_matriz(c);
-    }*/
-
-    if(world_rank == 0)
-    {
-        printf("Entrou 0\n");
-        somar_matrizes(a, b, c, world_rank);
-        printf("***Matriz C***\n");
-    	imprimir_matriz(c);
-        MPI_Send(&c, 1, MPI_INT, (world_rank + 1) % world_size, 0, MPI_COMM_WORLD);
-    } 
-    else if(world_rank == 1)
-    {
-        printf("Entrou 1\n");
-        MPI_Recv(&c, 1, MPI_INT, world_rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        somar_matrizes(a, b, c, world_rank);
-        printf("***Matriz C***\n");
-    	imprimir_matriz(c);
-        MPI_Send(&c, 1, MPI_INT, (world_rank + 1) % world_size, 0, MPI_COMM_WORLD);
-    } 
-    else if(world_rank == 2)
-    {
-        printf("Entrou 2\n");
-        MPI_Recv(&c, 1, MPI_INT, world_rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        somar_matrizes(a, b, c, world_rank);
-        printf("***Matriz C***\n");
-    	imprimir_matriz(c);
-        MPI_Send(&c, 1, MPI_INT, (world_rank + 1) % world_size, 0, MPI_COMM_WORLD);
-    } 
-    else if(world_rank == 3)
-    {
-        printf("Entrou 3\n");
-        MPI_Recv(&c, 1, MPI_INT, world_rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        somar_matrizes(a, b, c, world_rank);
-        printf("***Matriz C***\n");
-    	imprimir_matriz(c);
-        MPI_Send(&c, 1, MPI_INT, (world_rank + 1) % world_size, 0, MPI_COMM_WORLD);
     }
-
-    /*switch (world_rank)
-    {
-    case 0:
-        printf("Entrou 0");
-        MPI_Recv(&c, 1, MPI_INT, 3, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        somar_matrizes(a, b, c, world_rank);
-        printf("***Matriz C***\n");
-    	imprimir_matriz(c);
-        MPI_Send(&c, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
-        break;
-
-    case 1:
-        printf("Entrou 1");
-        MPI_Recv(&c, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        somar_matrizes(a, b, c, world_rank);
-        printf("***Matriz C***\n");
-    	imprimir_matriz(c);
-        MPI_Send(&c, 1, MPI_INT, 2, 0, MPI_COMM_WORLD);
-        break;
-    
-    case 2:
-        printf("Entrou 2");
-        MPI_Recv(&c, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        somar_matrizes(a, b, c, world_rank);
-        printf("***Matriz C***\n");
-    	imprimir_matriz(c);
-        MPI_Send(&c, 1, MPI_INT, 3, 0, MPI_COMM_WORLD);
-        break;
-    
-    case 3:
-        printf("Entrou 3");
-        MPI_Recv(&c, 1, MPI_INT, 2, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        somar_matrizes(a, b, c, world_rank);
-        printf("***Matriz C***\n");
-    	imprimir_matriz(c);
-        MPI_Send(&c, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-        break;
-
-    default:
-        break;
-    }*/
 
     MPI_Finalize();
 	return 0;
